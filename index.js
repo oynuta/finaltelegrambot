fdcd0e3061f3b55422bc5dc7e36ad74b575d1966
Oynuta <rzmetova>
2025-12-04 17:29:30 +0500

    asabbbb

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+import TelegramBot from "node-telegram-bot-api";
+import { config } from "dotenv";
+config();
 import "./src/bot/bot.js";
+const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 
+bot.on("message", (msg) => {
+  const chatId = msg.chat.id;
+  const firstname = msg.chat.first_name;
+  const text = msg.text;
 
-console.log("working....")
+  bot.sendMessage(chatId, `Assalamu Alaikum,welcomeeee)), ${firstname}`);
+  bot.sendMessage(chatId, `text: ${text}`);
+});
 
-
+console.log("working!");
--- src/bot/bot.js
+++ src/bot/bot.js
@@ -1,1 +1,13 @@
-console.log("Bot ishga tushdi...");
+import mongoose from "mongoose";
+
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log(`db IS CONNECTED!`);
+  })
+  .catch(() => {
+    console.log(`DB DISCONNNECTED!`);
+  });
+
+console.log("working!");
